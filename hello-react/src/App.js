dcf9aa5b3dbf7058ccadc38d4fda3443822d8c7e
dkkim <ekakth2458@gmail.com>
2022-03-24 13:42:58 +0900

    class component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg'; /*import 특저 파일을 불러 오는 것을 의미*/ 
-import './App.css';
+//import './App.css';
+import React,{Component} from'react';
 
-function App() {
-  const v_react = "React";
-  return (
-    <>
-      {/*주석입니다*/}
-      <h1 className="react-Test">{v_react} Test</h1>
-      /*이렇게 하면 주석 안됨*/ 
-    </> 
-  );
+class App extends Component{
+  render(){
+    const v_react =  "React";
+    return <h1 className="react-Test">{v_react} Test</h1>
+  }
 }
-
 export default App;
